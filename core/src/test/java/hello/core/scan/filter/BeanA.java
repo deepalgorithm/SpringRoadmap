c0bc02b33098f91b7de712e1eeb6e3844b568a6f
deepalgorithm <hamin.lim@gmail.com>
2024-02-06 09:46:51 +0900

    Spring Core - Basic

---
+++ core/src/test/java/hello/core/scan/filter/BeanA.java
@@ -0,0 +1,18 @@
+package hello.core.scan.filter;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.*;
+
+@MyIncludeComponent
+public class BeanA {
+
+
+}
